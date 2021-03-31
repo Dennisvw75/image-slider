4abbf4472fc438cc231766397d3c958fb22d705c
Dennis van Wetering <dvanwetering@gmail.com>
2021-03-31 18:09:55 +0200

    finished the image slider

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
+const imageContainer = document.querySelector('.image-container');
+
+const imagesArray = [
+  'images/image-1.jpg',
+  'images/image-2.jpg',
+  'images/image-3.jpg',
+  'images/image-4.jpg',
+  'images/image-5.jpg',
+  'images/image-6.jpg',
+  'images/image-7.jpg',
+  'images/image-8.jpg',
+  'images/image-9.jpg',
+  'images/image-10.jpg',
+];
+
+let next = 0;
+
+(function () {
+  const image = document.createElement('img');
+  image.classList.add('img');
+  image.src = imagesArray[next];
+  imageContainer.appendChild(image);
+})();
+
+const nextImage = function () {
+  const getImage = document.querySelector('img');
+  next++;
+  next = next % imagesArray.length;
+  getImage.src = imagesArray[next];
+
+  if (next === 9) {
+    btnRight.disabled = true;
+  } else {
+    btnRight.disabled = false;
+    btnLeft.disabled = false;
+  }
+};
+
+const prevImage = function () {
+  const getImage = document.querySelector('img');
+  next--;
+  next = next % imagesArray.length;
+  getImage.src = imagesArray[next];
+
+  if (next === 0) {
+    btnLeft.disabled = true;
+  } else {
+    btnLeft.disabled = false;
+    btnRight.disabled = false;
+  }
+};
+
+btnLeft.addEventListener('click', prevImage);
+
+btnRight.addEventListener('click', nextImage);
--- images/README.md
+++ images/README.md
@@ -1,3 +1,15 @@
 # Image Slider with HTML, CSS and Javascript
 
-## A beginner project
+## A beginner project.
+
+### Phots I used for this little project.
+
+1. Photo by ThisIsEngineering from Pexels
+2. Photo by Vlada Karpovich from Pexels
+3. Photo by cottonbro from Pexels
+4. Photo by ThisIsEngineering from Pexels
+5. Photo by Sora Shimazaki from Pexels
+6. Photo by olia danilevich from Pexels
+7. Photo by cottonbro from Pexels
+8. Photo by olia danilevich from Pexels
+9. Photo by luis gomes from Pexels
